6c06310a5b2dda6a6b90c8d2818d415515f5e89a
Peeraphat Thongfueng <peeraphat.thong@gmail.com>
2017-08-07 15:27:23 +0700

    Add String day

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const moment = require('moment')
 moment.locale('th')
 
 const dateString = moment('2017-05-23').format('Do MMMM YYYY')          // Date string
-
-const dateCustom = moment('12/07/2017', 'DD/MM/YYYY').format('Do MMMM YYYY')          // Date string
+const dateCustom = moment('12/08/2017', 'DD/MM/YYYY').format('dddd Do MMMM YYYY')          // Date string
 
 console.log('Date String', dateString)
 console.log('Date String Custom', dateCustom)
