66e31d86434a3d1ca73e8177970d4fbb09c3a568
Peeraphat Thongfueng <peeraphat.thong@gmail.com>
2017-08-07 14:51:13 +0700

    custom format date string

--- app.js
+++ app.js
@@ -3,4 +3,7 @@ moment.locale('th')
 
 const dateString = moment('2017-05-23').format('Do MMMM YYYY')          // Date string
 
+const dateCustom = moment('12/07/2017', 'DD/MM/YYYY').format('Do MMMM YYYY')          // Date string
+
 console.log('Date String', dateString)
+console.log('Date String Custom', dateCustom)
